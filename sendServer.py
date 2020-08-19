f0862ec58f40ed20b9d3bf4c00e99705b23ad58f
rul <derkdemons13@gmail.com>
2020-08-19 09:50:12 +0700

    ADDING SEND SERVER ERROR

--- sendServer.py
+++ sendServer.py
@@ -6,26 +6,65 @@ import requests_async as requests
 
 async def sendErrorDisplay():
     try:
-        errorBooking = "SELECT ID, URL FROM send_error WHERE SEND_STATUS = %s ORDER BY ID ASC LIMIT 1"
+        errorBooking = "SELECT ID, POLICE_NO, STATUS, URL FROM send_error WHERE SEND_STATUS = %s ORDER BY ID ASC LIMIT 1"
         cursor.execute(errorBooking, ["TO DISPLAY"])
         valErrorBooking = cursor.fetchone()
         idErrorBook = valErrorBooking[0]
-        urlErrorBook = valErrorBooking[1]
+        ErrorNoPol = valErrorBooking[1]
+        statErrorBook = valErrorBooking[2]
+        urlErrorBook = valErrorBooking[3]
         sendErrorBookStat = True
     except:
         idErrorBook = 'OFF'
         urlErrorBook = 'OFF'
+        ErrorNoPol = "OFF"
+        statErrorBook = "OFF"
         sendErrorBookStat = False
     if sendErrorBookStat:
-        try:
-            logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  SEND GET {urlErrorBook}')
-            rBookingError = await requests.get(urlErrorBook, timeout = 2)
-            logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  SUCCESCFULLY SEND GET TO DISPLAY')
-            sqldelBook = 'DELETE FROM send_error WHERE id = %s'
-            cursor.execute(sqldelBook, [idErrorBook,])
-            mydb.commit()
-        except:
-            logger.error(f'[PULL BOOKING] : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+        if statErrorBook == 'BOOKING':
+            try:
+                dataErrorBook = {'nopol':ErrorNoPol}
+                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  SEND POST {dataErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
+                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  SUCCESCFULLY SEND GET TO DISPLAY')
+                sqldelBook = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelBook, [idErrorBook,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL BOOKING] : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+        elif statErrorBook == 'START':
+            try:
+                dataErrorBook = {'State':1}
+                logger.info(f'[PULL START]    : [SEND DISPLAY]  SEND POST {dataErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
+                logger.info(f'[PULL START]    : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                sqldelBook = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelBook, [idErrorBook,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL START]   : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+        elif statErrorBook == 'ALARMCOUNT':
+            try:
+                dataErrorBook = {'alarm':1}
+                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  SEND POST {dataErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
+                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                sqldelBook = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelBook, [idErrorBook,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL COUNT]   : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+        elif statErrorBook == 'ALARMOFF':
+            try:
+                dataErrorBook = {'alarm':0}
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  SEND POST {dataErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)                
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                sqldelBook = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelBook, [idErrorBook,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL COUNT]   : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
     if idErrorBook != 'OFF':
         return False
     else:
