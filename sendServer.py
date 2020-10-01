79ad51f4c4d3acde519d04d6466e371d82f9a438
rul <derkdemons13@gmail.com>
2020-10-01 08:51:31 +0700

    UPDATE SERVER

--- sendServer.py
+++ sendServer.py
@@ -1,96 +1,136 @@
 import asyncio
-from gateway_dock import cursor, mydb, app, conn, cursorsql, logger
+from gateway_dock import cursor, mydb, app, logger
 from datetime import datetime, date, timedelta
 import MySQLdb
 import requests_async as requests
 
 async def sendErrorDisplay():
     try:
-        errorBooking = "SELECT ID, POLICE_NO, STATUS, URL FROM send_error WHERE SEND_STATUS = %s ORDER BY ID ASC LIMIT 1"
+        errorBooking = "SELECT ID, DOCK, POLICE_NO, STATUS, URL FROM send_error WHERE SEND_STATUS = %s ORDER BY ID ASC LIMIT 1"
         cursor.execute(errorBooking, ["TO DISPLAY"])
         valErrorBooking = cursor.fetchone()
         idErrorBook = valErrorBooking[0]
-        ErrorNoPol = valErrorBooking[1]
-        statErrorBook = valErrorBooking[2]
-        urlErrorBook = valErrorBooking[3]
+        DockErrorBook = valErrorBooking[1]
+        ErrorNoPol = valErrorBooking[2]
+        statErrorBook = valErrorBooking[3]
+        urlErrorBook = valErrorBooking[4]
         sendErrorBookStat = True
     except:
         idErrorBook = 'OFF'
+        DockErrorBook = 'OFF'
         urlErrorBook = 'OFF'
         ErrorNoPol = "OFF"
         statErrorBook = "OFF"
         sendErrorBookStat = False
     if sendErrorBookStat:
         if statErrorBook == 'BOOKING':
+            #ada dockCode
             try:
-                dataErrorBook = {'nopol':ErrorNoPol}
-                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  SEND POST {dataErrorBook}')
-                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
-                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  SUCCESCFULLY SEND GET TO DISPLAY')
+                dataErrorBook = {"nopol":f"{ErrorNoPol}"}
+                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL BOOKING]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] SUCCESCFULLY SEND GET TO DISPLAY')
                 sqldelBook = 'DELETE FROM send_error WHERE id = %s'
                 cursor.execute(sqldelBook, [idErrorBook,])
                 mydb.commit()
             except:
-                logger.error(f'[PULL BOOKING] : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+                logger.error(f'[PULL BOOKING] : [SEND DISPLAY]  [DOCK {DockErrorBook}] [ERROR] SEND TO DISPLAY ERROR')
         elif statErrorBook == 'START':
+            #ada dockCode
             try:
-                dataErrorBook = {'state':"1"}
-                logger.info(f'[PULL START]    : [SEND DISPLAY]  SEND POST {dataErrorBook}')
-                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
-                logger.info(f'[PULL START]    : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                dataErrorBook = {"state":"1"}
+                logger.info(f'[PULL START]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL START]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL START]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] SUCCESCFULLY SEND POST TO DISPLAY')
                 sqldelBook = 'DELETE FROM send_error WHERE id = %s'
                 cursor.execute(sqldelBook, [idErrorBook,])
                 mydb.commit()
             except:
-                logger.error(f'[PULL START]   : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+                logger.error(f'[PULL START]   : [SEND DISPLAY]  [DOCK {DockErrorBook}] [ERROR] SEND TO DISPLAY ERROR')
         elif statErrorBook == 'STOP':
+            #ada dockCode
             try:
-                dataErrorBook = {'state':"0"}
-                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  SEND POST {dataErrorBook}')
-                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
-                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                dataErrorBook = {"state":"0"}
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY] [DOCK {DockErrorBook}]  SUCCESCFULLY SEND POST TO DISPLAY')
                 sqldelBook = 'DELETE FROM send_error WHERE id = %s'
                 cursor.execute(sqldelBook, [idErrorBook,])
                 mydb.commit()
             except:
-                logger.error(f'[PULL STOP]    : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+                logger.error(f'[PULL STOP]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] [ERROR] SEND TO DISPLAY ERROR')
         elif statErrorBook == 'ALARMCOUNT':
+            #udah ada dock code nya
             try:
-                dataErrorBook = {'alarm':"1"}
-                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  SEND POST {dataErrorBook}')
-                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
-                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                dataErrorBook = {"alarm":"1"}
+                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL COUNT]    : [SEND DISPLAY]  [DOCK {DockErrorBook}] SUCCESCFULLY SEND POST TO DISPLAY')
                 sqldelBook = 'DELETE FROM send_error WHERE id = %s'
                 cursor.execute(sqldelBook, [idErrorBook,])
                 mydb.commit()
             except:
-                logger.error(f'[PULL COUNT]   : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+                logger.error(f'[PULL COUNT]   : [SEND DISPLAY] [DOCK {DockErrorBook}]  [ERROR] SEND TO DISPLAY ERROR')
         elif statErrorBook == 'ALARMOFF':
+            #ada dock code
             try:
-                dataErrorBook = {'alarm':"0"}
-                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  SEND POST {dataErrorBook}')
-                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 2)
-                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                dataErrorBook = {"alarm":"0"}
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL STOP]     : [SEND DISPLAY]  [DOCK {DockErrorBook}] SUCCESCFULLY SEND POST TO DISPLAY')
                 sqldelBook = 'DELETE FROM send_error WHERE id = %s'
                 cursor.execute(sqldelBook, [idErrorBook,])
                 mydb.commit()
             except:
-                logger.error(f'[PULL COUNT]   : [SEND DISPLAY]  SEND TO DISPLAY ERROR')
+                logger.error(f'[PULL COUNT]   : [SEND DISPLAY]  [DOCK {DockErrorBook}] [ERROR] SEND TO DISPLAY ERROR')
+        elif statErrorBook == 'DISABLE':
+            try:
+                dataErrorBook = {"active":"0"}
+                logger.info(f'[PULL DISABLE]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL DISABLE]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL DISABLE]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] SUCCESCFULLY SEND POST TO DISPLAY')
+                sqldelBook = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelBook, [idErrorBook,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL DISBALE] : [SEND DISPLAY]  [DOCK {DockErrorBook}] [ERROR] SEND TO DISPLAY ERROR')
+        elif statErrorBook == 'ENABLE':
+            try:
+                dataErrorBook = {"active":"1"}
+                logger.info(f'[PULL ENABLE]   : [SEND DISPLAY] [DOCK {DockErrorBook}] SEND POST {dataErrorBook}')
+                logger.info(f'[PULL ENABLE]   : [SEND DISPLAY] [DOCK {DockErrorBook}] SEND TO URL {urlErrorBook}')
+                rBookingError = await requests.post(urlErrorBook, json=dataErrorBook, timeout = 5)
+                logger.info(f'[PULL ENABLE]   : [SEND DISPLAY]  SUCCESCFULLY SEND POST TO DISPLAY')
+                sqldelBook = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelBook, [idErrorBook,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL ENABLE]  : [SEND DISPLAY]  [DOCK {DockErrorBook}] [ERROR] SEND TO DISPLAY ERROR')
     if idErrorBook != 'OFF':
+        mydb.commit()
         return False
     else:
+        mydb.commit()
         return True
 
 async def sendErrorTapIn():
     try:
-        errorHFServer = "SELECT ID, UID, DOCK, URL, TIME FROM send_error WHERE SEND_STATUS = %s ORDER BY ID ASC LIMIT 1"
+        errorHFServer = "SELECT ID, UID, DOCK,  STATUS, URL, TIME, TOTALTIME FROM send_error WHERE SEND_STATUS = %s ORDER BY ID ASC LIMIT 1"
         cursor.execute(errorHFServer, ["TAP START IN"])
         valErrorHFServer = cursor.fetchone()
         idErrorHF = valErrorHFServer[0]
         uidErrorHF = valErrorHFServer[1]
         DockErrorHF = valErrorHFServer[2]
-        TimeErrorHF = valErrorHFServer[4]
-        urlErrorHF = valErrorHFServer[3]
+        statErrorHF = valErrorHFServer[3]
+        TimeErrorHF = valErrorHFServer[5]
+        urlErrorHF = valErrorHFServer[4]
+        totalTImeErrorHF = valErrorHFServer[6]
         sendErrorHF = True
     except:
         idErrorHF = 'OFF'
@@ -98,22 +138,41 @@ async def sendErrorTapIn():
         DockErrorHF = 'OFF'
         TimeErrorHF = 'OFF'
         urlErrorHF = 'OFF'
+        statErrorHF = 'OFF'
+        totalTImeErrorHF = 'OFF'
         sendErrorHF = False
     if sendErrorHF:
-        dataErrorHF = {'uidRfid':uidErrorHF,
-                'dockCode':DockErrorHF,
-                'time':str(TimeErrorHF) }
-        try:
-            logger.info(f'[PULL HF]       :   [SEND SERVER] {dataErrorHF}')
-            rHFError = await requests.post(urlErrorHF, json=dataErrorHF,timeout = 2)
-            logger.info(f'[PULL HF]       :   [SEND SERVER] SUCCESCFULLY SEND TO SERVER')
+        if statErrorHF == 'TAP':
+            dataErrorHF = {'uid':uidErrorHF,
+                    'dockCode':DockErrorHF,
+                    'time':str(TimeErrorHF) }
+            try:
+                logger.info(f'[PULL HF]       :   [SEND SERVER] [DOCK {DockErrorHF}] {dataErrorHF}')
+                logger.info(f'[PULL HF]       :   [SEND SERVER] [DOCK {DockErrorHF}] {urlErrorHF}')
+                rHFError = await requests.post(urlErrorHF, json=dataErrorHF,timeout = 5)
+                logger.info(f'[PULL HF]       :   [SEND SERVER] [DOCK {DockErrorHF}] SUCCESCFULLY SEND TO SERVER')
 
-            sqldelErrorHF = 'DELETE FROM send_error WHERE id = %s'
-            cursor.execute(sqldelErrorHF, [idErrorHF,])
-            mydb.commit()
-        except:
-            logger.error(f'[PULL HF]      :   SEND TO SERVER ERROR')
+                sqldelErrorHF = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelErrorHF, [idErrorHF,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL HF]      :   [SEND SERVER] [DOCK {DockErrorHF}] [ERROR] SEND TO SERVER ERROR')
+        elif statErrorHF == 'TERPAL':
+            dataErrorHF = {'timeTerpal':totalTImeErrorHF }
+            try:
+                logger.info(f'[PULL HF]       :   [SEND SERVER] [DOCK {DockErrorHF}] {dataErrorHF}')
+                logger.info(f'[PULL HF]       :   [SEND SERVER] [DOCK {DockErrorHF}] {urlErrorHF}')
+                rHFError = await requests.post(urlErrorHF, json=dataErrorHF,timeout = 5)
+                logger.info(f'[PULL HF]       :   [SEND SERVER] [DOCK {DockErrorHF}] SUCCESCFULLY SEND TO SERVER')
+
+                sqldelErrorHF = 'DELETE FROM send_error WHERE id = %s'
+                cursor.execute(sqldelErrorHF, [idErrorHF,])
+                mydb.commit()
+            except:
+                logger.error(f'[PULL HF]      :   [SEND SERVER] [DOCK {DockErrorHF}] [ERROR] SEND TO SERVER ERROR')
     if idErrorHF != 'OFF':
+        mydb.commit()
         return False
     else:
+        mydb.commit()
         return True
